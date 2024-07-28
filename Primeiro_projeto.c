1b7c1c0a6e52960314098f438344fb233d8f0599
Sabrinafogliene <sabrina.castilho4@gmail.com>
2024-07-28 20:41:19 -0300

    Create Primeiro_projeto.c

---
+++ Primeiro_projeto.c
@@ -0,0 +1,15 @@
+// ***********************
+//
+//    Programa de teste
+//
+//
+// ***********************
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+  printf("OLA !!\n");
+  system("PAUSE");   
+  return 0;
+}
