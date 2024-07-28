b3d42b48755fd623bac532767858b8e9fe03ee54
Sabrinafogliene <sabrina.castilho4@gmail.com>
2024-07-28 20:42:44 -0300

    Create exercicio_4.cpp

---
+++ projetos/helloword/exercicio_4.cpp
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main (){
+    char escolha;
+    escolha = 'a';
+    escolha = escolha + 1;
+
+    printf("A variável escolha possui o valor: %d ", escolha);
+    return 0;
+}
