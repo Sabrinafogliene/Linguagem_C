7304d80b0012f7c5166835be14bbdc12dbb78c12
Sabrinafogliene <sabrina.castilho4@gmail.com>
2024-07-28 20:43:20 -0300

    Create exercicio_6.cpp

---
+++ projetos/helloword/exercicio_6.cpp
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+    int opcao;
+    
+    printf("Entre com um número inteiro:\n");
+    scanf("%d",&opcao);
+
+    if(opcao > 0) {
+        printf("\nNúmero positivo.\n");
+    } else if (opcao == 0) {
+        printf("\nNúmero nulo.\n");
+    
+    } else {
+        printf("\nNúmero negativo.\n");
+    }
+
+
+}
